808bb464d0cc872992bbbc60bcf73318eebb425a
KoushikaPriyanka <priyanka.p@voonik.com>
2017-07-09 00:19:22 +0530

    database and migration files

---
+++ db/migrate/20170708182206_create_colleges.rb
@@ -0,0 +1,14 @@
+class CreateColleges < ActiveRecord::Migration[5.0]
+  def change
+    create_table :colleges do |t|
+    	t.string :name
+    	t.string :email
+    	t.string :phone_number
+    	t.string :adddress
+    	t.string :city
+    	t.string :state
+    	t.string :country
+    	t.string :zipcode
+    end
+  end
+end
